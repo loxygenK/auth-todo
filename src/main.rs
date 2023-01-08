2aa76dabd95fd4837c14fa520b8fa7d284f71ce1
loxygenk <me@loxygen.dev>
2023-01-08 22:41:01 +0900

    feat: connect to the db, start API server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
+use api::start_api;
+use sea_orm::Database;
+
+use crate::api::ctx::Context;
+
+mod api;
 mod entity;
-    println!("Hello, world!");
+mod services;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    let db = Database::connect(std::env::var("DATABASE_URL").expect("DATABASE_URL to be served")).await?;
+
+    let context = Context::new(db);
+
+    start_api(context).await
 }
